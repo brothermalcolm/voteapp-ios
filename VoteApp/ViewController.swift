23f92986412bea094dbb88fd8aec9c091e91c166
Romans Karpelcevs <karpelcev@gmail.com>
2015-01-05 12:52:32 +0200

    Added faces with target actions

--- VoteApp/ViewController.swift
+++ VoteApp/ViewController.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
+private let kGoodVotesKey = "prefGoodKey"
+private let kNeutralVotesKey = "prefNeutralKey"
+private let kBadVotesKey = "prefBadKey"
+
 class ViewController: UIViewController {
+    @IBOutlet weak var sadFace: UIImageView!
+    @IBOutlet weak var neutralFace: UIImageView!
+    @IBOutlet weak var happyFace: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
 
+    @IBAction func redTouched() {
 
+    }
+
+    @IBAction func yellowTouched() {
+
+    }
+
+    @IBAction func greedTouched() {
+
+    }
 }
-
